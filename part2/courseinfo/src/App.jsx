b8fbf699c8fd95f09c865a7575dccbda0e8e313c
Azuria <achmadsyidiq@outlook.com>
2024-07-27 22:07:29 +0700

    Exercise 2.7

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -7,7 +7,25 @@ const Part = ({ part }) => (
 );
 
 const Content = ({ parts }) => {
+	/** Error handling if there are no parts in the course. */
+	if (!parts.part) return <p>no part available</p>;
 	return parts.map((part, index) => <Part part={part} key={index} />);
+};
+
+const TotalExercise = ({ parts }) => {
+	/** if total exercises of part is 0 */
+	if (!parts)
+		return (
+			<p>
+				<strong>total of 0 exercises</strong>
+			</p>
+		);
+	const total = parts.reduce((sum, part) => sum + part.exercise, 0);
+	return (
+		<p>
+			<strong>total of {total} exercises</strong>
+		</p>
+	);
 };
 
 const Course = ({ course }) => {
@@ -15,6 +33,7 @@ const Course = ({ course }) => {
 		<div>
 			<Header course={course.name} />
 			<Content parts={course.parts} />
+			<TotalExercise parts={course.parts} />
 		</div>
 	);
 };
